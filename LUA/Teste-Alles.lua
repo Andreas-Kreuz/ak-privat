9a6eb2f0cc2b5637c20f1b6abc1f1449ba601256
Andreas Kreuz <Andreas.Kreuz@endlich-zeit-haben.de>
2017-11-01 22:11:32 +0100

    Versuch alles zu testen

---
+++ LUA/Teste-Alles.lua
@@ -0,0 +1,7 @@
+-- Tests öffentliche Anlagen
+require 'ak.demo-anlagen.testen.Andreas_Kreuz-Lua-Testbeispiel-test'
+require 'ak.demo-anlagen.ampel.Ampeldemo-Grundmodelle-test'
+
+-- Tests private Anlagen
+require 'ak.anlagen.gruenberg-neustadt.Gruenberg-Neutstadt-DR-test'
+require 'ak.anlagen.die-moderne.Die-Moderne-07-test'
